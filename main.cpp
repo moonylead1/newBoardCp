741b305c8bfaed86c3661b39fd9e16e336872fdd
moonylead1 <ivankozulwork@gmail.com>
2023-10-24 00:57:26 +0300

    added commandQuit, boardShow func, some fixes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 
-vector <vector <int>> board;
+vector <vector <int>> board; // init of a board
 string command = ""; // string for command;
-                     //
-int currentMove = 1;
+int currentMove = 1; // first move by default is black
+
+void boardShow(vector<vector <int>> board){                           // show current board
+  for (int width = 0; width < board.size(); width++){
+    for (int length = 0; length < board.size(); length++){
+      cout << board[width][length] << " ";
+    }
+    cout << endl;
+  }
+}
 
 int stoneColorSelect(){
   if (currentMove % 2 == 0){
@@ -14,8 +23,9 @@ int stoneColorSelect(){
   }
   return 1;
 }
-vector <vector <int>> generateBoard(int length, int width){
-  vector<vector<int>> newBoard( length , vector<int> (width, 0));
+
+vector <vector <int>> boardGenarate(int length, int width){           // generate board
+  vector<vector<int>> newBoard( length , vector<int> (width, 0));     // clear board with zero init
   return newBoard;
 }
 
@@ -26,28 +36,40 @@ void boardSetup(){ //make a new board for new game
   cin >> task;
   switch (task) {
     case 1:
-      board = generateBoard(9, 9);
+      board = boardGenarate(9, 9);
       break;
     case 2:
-      board = generateBoard(13, 13);
+      board = boardGenarate(13, 13);
       break;
     case 3:
-      board = generateBoard(19, 19);
+      board = boardGenarate(19, 19);
     default:
-      
-      break;
+      cout << "Warning: no board was generated. Proceed? [y] - yes, [n] - no.";
+      string afterCall;
+      cin >> afterCall;
+      if (afterCall == "n" or afterCall == "N"){
+        break;
+      }
   }
-} 
-
+}
 
 void boardMove (){ //moves
   int x, y;
   cout << "Enter coordinates x, y: ";
   cin >> x >> y;
-  board[x][y] = stoneColorSelect();
+  board[x-1][y-1] = stoneColorSelect();
   currentMove++;
+  cout << "Current move: " << currentMove-1 << endl;
+  boardShow(board);
 }
-
+void commandQuit(){
+  cout << "Quit command requested. Proceed? [Y] - yes, [N] - no : ";
+  string afterCall;
+  cin >> afterCall;
+  if (afterCall == "Y" or afterCall == "y"){
+    exit(1);
+  }
+}
 void commandList(string command) { //command list executable;
   if (command == "open new" or command == "on"){
     boardSetup();
@@ -55,17 +77,17 @@ void commandList(string command) { //command list executable;
   if (command == "move" or command == "m"){
     boardMove();
   }
+  if (command=="quit" or command == "q"){
+    commandQuit();
+  }
   //...
 }
 
-int main (int argc, char *argv[]) {  
+int main (int argc, char *argv[]) {
   while (true) {
+    cout << "cmd line: ";
     cin >> command;
-    if (command==""){
-      cout << "no command was written";
-      break;
-    }
-    commandList(command);
+    commandList(command); //excecutes command from list of avaible
   }
   return 0;
 }
