0db6a9accabbb85b1bb1e58fc33d74c16b975593
moonylead1 <ivankozulwork@gmail.com>
2023-10-23 22:17:17 +0300

    README.md Specs + main.cpp base idea of code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # newBoardCp
- Terminal go
+ Description: board with multiple functions to play go in terminal.
+ Possible add-ons: ui display.
+ Current to-do:
+    1. Make a good working placement setting for the multiple boards
+    2. Implement menu
+    3. Make a back moves
+    4. Multiplayer staff
+    5. Ui staff (i still dont know anything about it)
--- main.cpp
+++ main.cpp
@@ -1,19 +1,61 @@
-include <iostream>
-include <math.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
 using namespace std;
 
+vector <vector <int>> board;
 string command = ""; // string for command;
-void boardMove (){
+                     //
+int currentMove = 1;
+
+int stoneColorSelect(){
+  if (currentMove % 2 == 0){
+    return 2;
+  }
+  return 1
+}
+vector <vector <int>> generateBoard(length, width){
+  vector<vector<int>> newBoard( length , vector<int> (width, 0));
+  return newBoard;
+}
+
+
+void boardSetup(){ //make a new board for new game
+  int task;
+  cout << "1. (std9x9) 9x9 board;\n2. (std13x13) 13x13 board;\n3. (std19x19) 19x19 board\nEnter 1-3: ";
+  cin >> task;
+  switch (task) {
+    case 1:
+      board = generateBoard(9, 9);
+      break;
+    case 2:
+      board = generateBoard(13, 13);
+      break;
+    case 3:
+      board = generateBoard(19, 19);
+    default:
+      
+      break;
+  }
+} 
+
+
+void boardMove (){ //moves
   int x, y;
   cout << "Enter coordinates x, y: "
   cin >> x >> y;
-
+  board[x][y] = stoneColorSelect();
+  currentMove++;
 }
 
 void commandList() { //command list executable;
-  if (command == "move"){
+  if (command == "open new" or command == "on"){
+    boardSetup();
+  }
+  if (command == "move" or command == "m"){
     boardMove();
   }
+  //...
 }
 
 int main (int argc, char *argv[]) {  
